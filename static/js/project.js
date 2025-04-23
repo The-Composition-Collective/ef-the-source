db57c32bb72f735090d01fc7e561304c415a6bbb
Jay Pilgreen <jaypilgreen@gmail.com>
2025-04-22 22:05:12 -0500

    client feedback

--- static/js/project.js
+++ static/js/project.js
@@ -1,5 +1,5 @@
 /*
- * Intersection Observers
+ * Videos
  */
 
 const videoObserver = new IntersectionObserver(entries => {
@@ -14,6 +14,14 @@ const videoObserver = new IntersectionObserver(entries => {
   threshold: 0.5
 });
 
+document.querySelectorAll("video").forEach(ele => {
+  videoObserver.observe(ele);
+});
+
+/*
+ * Panel animations
+ */
+
 const animationObserver = new IntersectionObserver(entries => {
   entries.forEach(entry => {
     entry.target.classList.toggle("show", entry.isIntersecting);
@@ -21,18 +29,6 @@ const animationObserver = new IntersectionObserver(entries => {
 }, {
   threshold: 0.1
 });
-
-/*
- * Panels
- */
-
-document.querySelectorAll("video").forEach(ele => {
-  videoObserver.observe(ele);
-});
-
-/*
- * Facts
- */
 
 document.querySelectorAll(".animate-group").forEach(ele => {
   let delay = 0.6;
@@ -44,3 +40,33 @@ document.querySelectorAll(".animate-group").forEach(ele => {
 
   animationObserver.observe(ele);
 });
+
+/*
+ * Strips
+ */
+
+const stripObserver = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if(entry.isIntersecting) {
+      entry.target.intervalId = setInterval(next.bind(entry.target), 5000);
+    } else {
+      clearInterval(entry.target.intervalId);
+    }
+  });
+}, {
+  threshold: 1
+});
+
+document.querySelectorAll(".strip").forEach(ele => {
+  stripObserver.observe(ele);
+});
+
+function next() {
+  const current = Array.from(this.children).find(ele => {
+    const bbox = ele.getBoundingClientRect();
+    return bbox.left >= 0 && bbox.right <= window.innerWidth;
+  });
+
+  const next = current.nextElementSibling;
+  next?.scrollIntoView({ behavior: "smooth" });
+}
